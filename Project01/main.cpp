22739248bec60914f972332c77c59e85423eb885
ITSTEP\st <st>
2024-02-27 17:50:40 +0300

    .

--- Project01/main.cpp
+++ Project01/main.cpp
@@ -1,45 +1,36 @@
 #include <iostream>
-#include <string>
+#include <ctime>
 
 using namespace std;
 
 
 int main() {
+	
+	srand(time(NULL));
 
-	int size = 5;
+	int size;
 
-	//do {
-	//	system("cls");
-	//	cout << "Input size of array: ";
-	//	cin >> size;
-	//} while (size <= 0);
+	do {
+		system("cls");
+		cout << "Input size of array: ";
+		cin >> size;
+	} while (size <= 0);
 
 	int* array = new int[size];  // dynamic array
 
-	//for (int i = 0; i < size; i++) // i(0, 1, 2, 3, 4)
-	//{
-	//	array[i] = 10; // --> *(array + i)
-	//}
+	for (int i = 0; i < size; i++)
+	{
+		*(array + i) = rand() % 10;
+	}
 
+	cout << "\nElements of dynamic array: ";
 
-	//string result = "";
-	//for (int j = 0; j < size; j++)
-	//{
-	//	result += array[j] + " ";
-	//}
+	for (int j = 0; j < size; j++)
+	{
+		cout << *(array + j) << " ";
+	}
 
-
-	*(array + 0) = 11;
-	*(array + 1) = 22;
-	*(array + 2) = 33;
-	*(array + 3) = 44;
-	*(array + 4) = 55;
-
-	cout << array + 0 << " - " << *(array + 0) << endl;
-	cout << array + 1 << " - " << *(array + 1) << endl;
-	cout << array + 2 << " - " << *(array + 2) << endl;
-	cout << array + 3 << " - " << *(array + 3) << endl;
-	cout << array + 4 << " - " << *(array + 4) << endl;
+	cout << endl;
 
 	return 0;
 }
